cd8d665ed85b7181582f772a98d573ead6f49a28
korahhira <z1keldanulo281@gmail.com>
2026-05-15 04:22:05 +0300

    cors fix

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ if (!fs.existsSync(uploadDir)) {
 // CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://cbc-nine.vercel.app/"],
+    origin: ["http://localhost:5173", "https://cbc-oman.onrender.com"],
     credentials: true,
   }),
 );
