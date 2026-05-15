6c9dd4bc328176a9cb66b4cd8832c6d98bc8939a
korahhira <z1keldanulo281@gmail.com>
2026-05-15 04:37:25 +0300

    fix server export

--- server/bin/runners/http.js
+++ server/bin/runners/http.js
@@ -1,24 +1,11 @@
 import app from "../../server.js";
 
-export default function httpRunner() {
+const httpRunner = () => {
   const PORT = process.env.PORT || 4000;
 
-  const server = app.listen(PORT, "0.0.0.0", () => {
-    console.log(`🚀 Сервер запущено на порту ${PORT}`);
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server on ${PORT}`);
   });
+};
 
-  server.on("error", (err) => {
-    console.error("❌ Server error:", err);
-    process.exit(1);
-  });
-
-  process.on("uncaughtException", (err) => {
-    console.error("❌ Uncaught exception:", err);
-    process.exit(1);
-  });
-
-  process.on("unhandledRejection", (err) => {
-    console.error("❌ Unhandled rejection:", err);
-    process.exit(1);
-  });
-}
+export default httpRunner;
